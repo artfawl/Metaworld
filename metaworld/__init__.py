f7be74c0429712b8c907f7be0f98413df93fe6ba
Andrei Polubarov <stark050600@mail.ru>
2025-06-27 14:34:43 +0300

    debug 5

--- metaworld/__init__.py
+++ metaworld/__init__.py
@@ -213,7 +213,8 @@ class MetaWorldEnv(gymnasium.Env):
         self.render_mode = render_mode
 
     def reset(self, seed=None, options=None):
-        self.meta_env._freeze_rand_vec = False
+        print("MetaWorldEnv reset")
+        # self.meta_env._freeze_rand_vec = False
         self.meta_env._set_task_called = True
         return self.meta_env.reset().astype(np.float32), {}
 
